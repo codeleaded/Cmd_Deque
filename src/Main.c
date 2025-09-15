ba2fdd5d87737560c27bb97973fd6c369ade07b8
codeleaded <codeleaded@gmail.com>
2025-09-15 03:05:31 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -2,5 +2,15 @@
 
 int main(int argc,const char *argv[]){
     
+    Deque d = Deque_New(sizeof(int));
+
+    Deque_Push(&d,(int[]){ 1 });
+    Deque_Push(&d,(int[]){ 2 });
+    Deque_Push(&d,(int[]){ 3 });
+    Deque_Push(&d,(int[]){ 4 });
+    Deque_Push(&d,(int[]){ 5 });
+
+    Deque_Free(&d);
+
     return 0;
 }
